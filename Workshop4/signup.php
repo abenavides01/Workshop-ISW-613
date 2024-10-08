c8b53f015c6654a2709bc4c66a908afe00ffa439
abenavides01 <andrepaobena@gmail.com>
2024-10-07 22:38:21 -0600

    signup listo

--- Workshop4/signup.php
+++ Workshop4/signup.php
@@ -1,33 +1,50 @@
 <?php
-  include('utils/functions.php');
+include('utils/functions.php');
+
+$provinces = getProvinces();
+$error_msg = '';
+if (isset($_GET['error'])) {
+    $error_msg = $_GET['error'];
+}
 ?>
-<?php require('inc/header.php')?>
-  <div class="container-fluid">
+<?php require('inc/header.php') ?>
+<div class="container-fluid">
     <div class="jumbotron">
-      <h1 class="display-4">Users</h1>
-      <p class="lead">List of users</p>
-      <hr class="my-4">
+        <h1 class="display-4">Signup</h1>
+        <p class="lead">This is the signup process</p>
+        <hr class="my-4">
     </div>
-    <table class="table">
-      <thead>
-        <tr>
-          <th>First Name</th>
-          <th>Lane</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td scope="row"></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td scope="row"></td>
-          <td></td>
-          <td></td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+    <form method="post" action="actions/signup.php">
+        <div class="error">
+            <?php echo $error_msg; ?>
+        </div>
+        <div class="form-group">
+            <label for="first-name">First Name</label>
+            <input id="first-name" class="form-control" type="text" name="firstname">
+        </div>
+        <div class="form-group">
+            <label for="last-name">Last Name</label>
+            <input id="last-name" class="form-control" type="text" name="lastname">
+        </div>
+        <div class="form-group">
+            <label for="province">Provincia</label>
+            <select id="province" class="form-control" name="province_id">
+                <?php
+                foreach ($provinces as $id => $province) {
+                    echo "<option value=\"$id\">$province</option>";
+                }
+                ?>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="email">Email Address</label>
+            <input id="email" class="form-control" type="text" name="username">
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input id="password" class="form-control" type="password" name="password">
+        </div>
+        <button type="submit" class="btn btn-primary"> Sign up </button>
+    </form>
+</div>
 <?php require('inc/footer.php');
